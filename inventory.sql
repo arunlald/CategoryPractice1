f20d31c157e9376394f827b36d4a7b9f547d0530
arunlald <arunlald18@gmail.com>
2022-07-16 15:07:11 -0700

    commit for deploy

---
+++ inventory.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 16, 2022 at 11:58 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `catcode` varchar(5) NOT NULL,
+  `catdesc` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`catcode`, `catdesc`) VALUES
+('104', 'Gift Section'),
+('1035', 'Toiletries'),
+('102', 'Kitchenware'),
+('1005', 'Dog food'),
+('1002', 'Mens section'),
+('1111', 'Ozzy');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/javat/Category.java
@@ -0,0 +1,23 @@
+package javat;
+
+public class Category {
+
+    private String catcode;
+    private String catdesc;
+
+    public void setCatcode(String catcode) {
+        this.catcode = catcode;
+    }
+
+    public void setCatdesc(String catdesc) {
+        this.catdesc = catdesc;
+    }
+
+    public String getCatcode() {
+        return catcode;
+    }
+
+    public String getCatdesc() {
+        return catdesc;
+    }
+}
